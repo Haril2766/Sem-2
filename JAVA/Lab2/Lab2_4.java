612b907b32b50288a641b217145cd35f63df59bf
Haril2766 <parmarharil2766@gmail.com>
2024-12-10 16:12:26 +0530

    10-12-2024

---
+++ JAVA/Lab2/Lab2_4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Lab2_4 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a radius:");
+        double radius=sc.nextDouble();
+        double n=3.14;
+        double CircleArea=n*radius*radius;
+        System.out.println("The area of circle is: "+CircleArea);
+        
+        
+
+    }
+    
+}
+
